c481e69cec208ea4d50b51d51f6eee3c7b82ef25
pjvan_thof <pjrvanthof@gmail.com>
2017-09-18 13:40:12 +0200

    WIP

--- biopet-tool-utils/src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
+++ biopet-tool-utils/src/main/scala/nl/biopet/utils/tool/ToolCommand.scala
@@ -19,36 +19,7 @@ import nl.biopet.utils.Logging
 /**
   * Trait for biopet tools, sets some default args
   */
-trait ToolCommand {
+trait ToolCommand extends Logging {
 
   def main(args: Array[String])
-
-  protected type Args
-  protected type OptParser <: AbstractOptParser[Args]
-}
-
-/**
-  * Abstract opt parser to add default args to each biopet tool
-  */
-abstract class AbstractOptParser[T](cmdName: String) extends scopt.OptionParser[T](cmdName) {
-  opt[String]('l', "log_level") foreach { x =>
-    x.toLowerCase match {
-      case "debug" => Logging.logger.setLevel(org.apache.log4j.Level.DEBUG)
-      case "info" => Logging.logger.setLevel(org.apache.log4j.Level.INFO)
-      case "warn" => Logging.logger.setLevel(org.apache.log4j.Level.WARN)
-      case "error" => Logging.logger.setLevel(org.apache.log4j.Level.ERROR)
-      case _ =>
-    }
-  } text "Level of log information printed. Possible levels: 'debug', 'info', 'warn', 'error'" validate {
-    case "debug" | "info" | "warn" | "error" => success
-    case _ => failure("Log level must be <debug/info/warn/error>")
-  }
-  opt[Unit]('h', "help") foreach { _ =>
-    System.err.println(this.usage)
-    sys.exit(1)
-  } text "Print usage"
-  opt[Unit]('v', "version") foreach { _ =>
-    System.err.println("Version: " + Option(getClass.getPackage.getImplementationVersion).getOrElse("unpackaged"))
-    sys.exit(1)
-  } text "Print version"
 }
